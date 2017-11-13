4bf9ed4aa6f925fd656c364e214b108fdb9c5296
kameyamak0223 <kameyamak0223@gmail.com>
2017-11-14 03:17:36 +0900

    autoload読み込み修正

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/vendor/autload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 $httpClient = new \LINE\LINEBot\HTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 $bot = new \LINE\LINEBot($httpClient, ['hannelSecret' => getenv('CHANNEL_SECRET')]);
