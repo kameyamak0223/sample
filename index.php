c83430e10cf4c4640356bc611746d65836031935
kameyamak0223 <kameyamak0223@gmail.com>
2017-11-14 03:23:13 +0900

    修正

--- index.php
+++ index.php
@@ -2,20 +2,20 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$httpClient = new \LINE\LINEBot\HTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-$bot = new \LINE\LINEBot($httpClient, ['hannelSecret' => getenv('CHANNEL_SECRET')]);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 
 $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 try{
     $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 }catch(\LINE\LINEBot\Exception\InvalidSignatureException $e){
-    error_log("parseEvenRequest failed. InvalidSignatureExecption =>" . var_export($e, true));
+    error_log("parseEventRequest failed. InvalidSignatureExecption => " . var_export($e, true));
 }catch (\LINE\LINEBot\Exception\UnknowenEventTypeException $e){
-    error_log("parseEventRequest failed. UnknownEventTypeException =>" . var_export($e, true));
+    error_log("parseEventRequest failed. UnknownEventTypeException => " . var_export($e, true));
 }catch (\LINE\LINEBot\Exception\UnknowenMessageTypeException $e){
-    error_log("parseEventRequest failed UnknownMessageTypeException =>" . var_export($e, true));
+    error_log("parseEventRequest failed UnknownMessageTypeException => " . var_export($e, true));
 }catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e){
-    error_log("parseEventRequest failed. InvalidEventRequestException =>" . var_export($e, true));
+    error_log("parseEventRequest failed. InvalidEventRequestException => " . var_export($e, true));
 }
 
 foreach($events as $event){
