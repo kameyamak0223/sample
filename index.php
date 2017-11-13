2089f73f30599cb70d794922d2bc431430e0a88e
kameyamak0223 <kameyamak0223@gmail.com>
2017-11-14 00:17:24 +0900

    返信記述

--- index.php
+++ index.php
@@ -7,7 +7,25 @@ $bot = new \LINE\LINEBot($httpClient, ['hannelSecret' => getenv('CHANNEL_SECRET'
 
 $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 try{
-	$events = $bot->parseEventRequest();
+    $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+}catch(\LINE\LINEBot\Exception\InvalidSignatureException $e){
+    error_log("parseEvenRequest failed. InvalidSignatureExecption =>" . var_export($e, true));
+}catch (\LINE\LINEBot\Exception\UnknowenEventTypeException $e){
+    error_log("parseEventRequest failed. UnknownEventTypeException =>" . var_export($e, true));
+}catch (\LINE\LINEBot\Exception\UnknowenMessageTypeException $e){
+    error_log("parseEventRequest failed UnknownMessageTypeException =>" . var_export($e, true));
 }catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e){
+    error_log("parseEventRequest failed. InvalidEventRequestException =>" . var_export($e, true));
+}
 
+foreach($events as $event){
+    if(!($event instanceof \LINE\LINEBot\Event\MessageEvent)){
+        error_log('Non message event has come');
+        continue;
+    }
+    if(!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)){
+        error_log('Non text message has come');
+        continue;
+    }
+    $bot->replyText($event->getReplyToken(), $event->getText());
 }
