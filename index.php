6b0f42388f9d1d13eff94871a5cd3ffed97ca803
kameyamak0223 <kameyamak0223@gmail.com>
2017-11-14 17:28:58 +0900

    スタンプ返信

--- index.php
+++ index.php
@@ -39,7 +39,9 @@ foreach($events as $event){
     
     //replyTextMessage($bot, $event->getReplyToken(), "TextMessage");
     //replyImageMessage($bot, $event->getReplyToken(), "https://" . $_SERVER["HTTP_HOST"] . "/imgs/original.jpg", "https://" . $_SERVER["HTTP_HOST"] . "/imgs/preview.jpg");
-    replyLocationMessage($bot, $event->getReplyToken(), "LINE", "東京都渋谷区2-21-1 ヒカリエ27階", 35.659025, 139.703473);
+    //replyLocationMessage($bot, $event->getReplyToken(), "LINE", "東京都渋谷区2-21-1 ヒカリエ27階", 35.659025, 139.703473);
+    
+    replyStickerMessage($bot, $event->getReplyToken(), 1, 1);
 }
 
 //テキストの返信
@@ -64,4 +66,12 @@ function replyLocationMessage($bot, $replyToken, $title, $address, $lat, $lon){
    if(!$response->isSucceeded()){
        error_log('Failed!' . $response->getHTTPStatus . ' ' . $response->getRawBody());
    }
+}
+
+//スタンプ返信
+function replyStickerMessage($bot, $replyToken, $packageId, $stickerId){
+    $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder($packageId, $stickerId));
+    if(!$response->isSucceeded()){
+        error_log('Failed!' . $response->getHTTPStatus . ' ' . $response->getRawBody());
+    }
 }
