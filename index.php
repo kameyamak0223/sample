b23e93eb35a8877332b7ee8bc13fe1971c4e926b
kameyamak0223 <kameyamak0223@gmail.com>
2017-11-14 17:16:17 +0900

    位置情報

--- index.php
+++ index.php
@@ -38,9 +38,11 @@ foreach($events as $event){
 //            );
     
     //replyTextMessage($bot, $event->getReplyToken(), "TextMessage");
-    replyImageMessage($bot, $event->getReplyToken(), "https://" . $_SERVER["HTTP_HOST"] . "/imgs/original.jpg", "https://" . $_SERVER["HTTP_HOST"] . "/imgs/preview.jpg");
+    //replyImageMessage($bot, $event->getReplyToken(), "https://" . $_SERVER["HTTP_HOST"] . "/imgs/original.jpg", "https://" . $_SERVER["HTTP_HOST"] . "/imgs/preview.jpg");
+    replyLocationMessage($bot, $event->getReplyToken(), "LINE", "東京都渋谷区2-21-1 ヒカリエ27階", 35.659025, 139.703473);
 }
 
+//テキストの返信
 function replyTextMessage($bot, $replyToken, $text){
     $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
     if(!$response->isSucceeded()){
@@ -48,9 +50,18 @@ function replyTextMessage($bot, $replyToken, $text){
     }
 }
 
+//画像の返信
 function replyImageMessage($bot, $replyToken, $originalImageUrl, $previewImageUrl){
     $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($originalImageUrl, $previewImageUrl));
     if(!$response->isSucceeded()){
         error_log('Failed' . $response->getHTTPStatus . ' ' . $response->getRawBody());
     }
+}
+
+//位置情報の返信
+function replyLocationMessage($bot, $replyToken, $title, $address, $lat, $lon){
+   $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder($title, $address, $lat, $lon));
+   if(!$response->isSucceeded()){
+       error_log('Failed!' . $response->getHTTPStatus . ' ' . $response->getRawBody());
+   }
 }
